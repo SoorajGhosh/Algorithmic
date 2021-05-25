cf2e3b93cad589d6735cd3003a2a0e351175b994
SoorajGhosh <soorajghosh78@gmail.com>
2021-05-26 03:24:56 +0530

    Asset Modules set for file handling.

--- webpack.common.js
+++ webpack.common.js
@@ -13,6 +13,14 @@ module.exports = {
         test: /\.scss$/i,
         use: ['style-loader', 'css-loader',"sass-loader"],
       },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: "asset/resource",
+      }
     ],
   },
 	plugins: [
