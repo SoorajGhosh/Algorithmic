a5b25fef1621da3134671a2eef486a0ea8f003e6
SoorajGhosh <soorajghosh78@gmail.com>
2021-06-06 03:54:36 +0530

    Bouncing ball animation created

---
+++ src/js/Canvas.js
@@ -0,0 +1,24 @@
+import "../css/index.css";
+
+class CanvasObj{
+    constructor (domCanvas) {
+        this.width = domCanvas.width;
+        this.height = domCanvas.height;
+        this.ctx = domCanvas.getContext("2d");
+    }
+
+}
+
+const canvasEl = document.querySelector("#canvas");
+
+const updateCanvas = () => {
+    canvasEl.width = window.innerWidth;
+    canvasEl.height = (window.innerHeight<850) ? window.innerWidth : window.innerHeight;
+};
+
+updateCanvas() 
+window.addEventListener('resize', updateCanvas)  
+// canvasEl.width = window.innerWidth;
+// canvasEl.height = window.innerHeight;
+export const canvas = new CanvasObj(canvasEl);
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,15 @@
 import "../css/index.css";
+import {canvas} from "./Canvas.js";
+import {game_of_life} from "./algorithms/GameOfLife"
 
-console.log("hello11")
 
+const animate = () => {
+    canvas.ctx.clearRect(0,0,canvas.width,canvas.height);
+    game_of_life()
+    window.requestAnimationFrame(animate);
+}
+
+animate()
 
 // Hot Module Replacement
 if (module && module.hot) module.hot.accept()
